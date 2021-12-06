c47c55a83ead544784e85c8001f483ccf5b5731e
noamok2003 <94392790+noamok2003@users.noreply.github.com>
2021-12-06 15:52:07 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
 // JavaScript Document
+var hamburgerbutton = document.querySelector("#hamburger")
+var nav = document.querySelector("#nav")
+var kruisbutton = document.querySelector("#kruis")
+
+hamburgerbutton.addEventListener('click',active)
+kruisbutton.addEventListener('click',weg)
+
+function active(){
+    nav.classList.add('toonmenu')
+}
+
+function weg(){
+    nav.classList.remove('toonmenu')
+}
